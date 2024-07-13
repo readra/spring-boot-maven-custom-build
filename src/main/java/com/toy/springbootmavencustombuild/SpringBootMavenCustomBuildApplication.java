651f54471ce5dbf73e91602186eeece74055d824
readra <yong6302@gmail.com>
2024-07-13 23:56:13 +0900

    1. Project Setting commit

---
+++ src/main/java/com/toy/springbootmavencustombuild/SpringBootMavenCustomBuildApplication.java
@@ -0,0 +1,13 @@
+package com.toy.springbootmavencustombuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenCustomBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenCustomBuildApplication.class, args);
+	}
+
+}
