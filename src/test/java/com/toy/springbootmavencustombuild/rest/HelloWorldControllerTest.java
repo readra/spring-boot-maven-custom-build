918f0f71cf20738f75d77aa70950addeaacbeb1f
readra <yong6302@gmail.com>
2024-07-17 22:29:09 +0900

    1. Spring Boot + Maven 환경에서 커스텀 빌드하기 (2) > Common API 테스트 코드 작성

--- src/test/java/com/toy/springbootmavencustombuild/rest/HelloWorldControllerTest.java
+++ src/test/java/com/toy/springbootmavencustombuild/rest/HelloWorldControllerTest.java
@@ -1,4 +1,25 @@
 package com.toy.springbootmavencustombuild.rest;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloWorldController.class)
 public class HelloWorldControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Hello World")
+    void helloWorldTest() throws Exception {
+        mockMvc.perform(get("/v1/hello-world"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(HelloWorldController.HELLO_WORLD));
+    }
 }
