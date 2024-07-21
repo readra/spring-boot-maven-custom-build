9c3cfa0bde011f99adc4d58a32faeec9d3b5a37c
readra <yong6302@gmail.com>
2024-07-21 20:31:51 +0900

    1. Spring Boot + Maven 환경에서 커스텀 빌드하기 (2) > Common API Rest Docs 적용

--- src/test/java/com/toy/springbootmavencustombuild/rest/HelloWorldControllerTest.java
+++ src/test/java/com/toy/springbootmavencustombuild/rest/HelloWorldControllerTest.java
@@ -1,13 +1,32 @@
 package com.toy.springbootmavencustombuild.rest;
 
+import com.toy.springbootmavencustombuild.service.HelloWorldService;
+import org.junit.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -15,16 +34,55 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  *
  * @author readra
  */
+@AutoConfigureRestDocs
 @WebMvcTest(HelloWorldController.class)
 public class HelloWorldControllerTest {
-    @Autowired
-    MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	@MockBean
+	private HelloWorldService helloWorldService;
 
-    @Test
-    @DisplayName("Hello World")
-    void helloWorldTest() throws Exception {
-        mockMvc.perform(get("/v1/hello-world"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(HelloWorldController.HELLO_WORLD));
-    }
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+				.apply(documentationConfiguration(new JUnitRestDocumentation()))
+				.build();
+	}
+
+	@Test
+	@DisplayName("Hello World")
+	void helloWorldTest() throws Exception {
+		final int code = 100;
+		final String result = String.format(HelloWorldService.HELLO_WORLD, code);
+
+		// given
+		given(helloWorldService.helloWorld(anyInt())).willReturn(result);
+
+		// when
+		ResultActions resultActions = mockMvc.perform(
+				RestDocumentationRequestBuilders.get("/v1/hello-world/{code}", code)
+						.contentType(MediaType.APPLICATION_JSON)
+		);
+
+		// then
+		resultActions.andExpect(status().isOk())
+				.andDo(
+						document(
+								"Hello World",
+								preprocessRequest(prettyPrint()),
+								preprocessResponse(prettyPrint()),
+								pathParameters(
+										parameterWithName("code").description("코드")
+								),
+								responseFields(
+										fieldWithPath("message").type(JsonFieldType.STRING).description("메시지"),
+										fieldWithPath("code").type(JsonFieldType.NUMBER).description("코드")
+								)
+						)
+
+				)
+				.andDo(print());
+	}
 }
